a56a927a848563cad18dd4686e479e572ae52bf6
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2023-10-14 12:01:11 -0400

    Buttons

---
+++ idiotsandwich/html/js/input.js
@@ -0,0 +1,25 @@
+function addInput() {
+    let base = document.createElement("div");
+    base.classList.add("meal-input");
+
+    let name = document.createElement("h3");
+    name.innerText = "Meal";
+    base.appendChild(name);
+
+    let close = document.createElement("span");
+    close.classList = "close-button";
+    close.innerHTML = "&times";
+    const me = base;
+    close.onclick = function() {
+        document.getElementById("foodList").removeChild(me);
+    }
+    base.appendChild(close);
+
+    let input = document.createElement("input");
+    input.type = "text";
+    base.appendChild(input);
+
+    document.getElementById("foodList").appendChild(base);
+}
+addInput();
+console.log("owo")
