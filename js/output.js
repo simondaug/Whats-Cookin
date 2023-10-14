ccaed5579fa6e61b575c6897d8d0c46b23b3518a
Simon Daugherty <67810892+simondaug@users.noreply.github.com>
2023-10-14 16:39:30 -0400

    added loading stage

--- js/output.js
+++ js/output.js
@@ -1,8 +1,21 @@
 function cleanUp() {
     document.getElementById("foodInput").style.display = "none";
-    document.getElementById("foodOutput").style.display = "block";
+
+    document.getElementById("loading").style.display = "flex";
+    
+    setTimeout(() => {
+        document.getElementById("loading").style.display = "none";
+
+        document.getElementById("foodOutput").style.display = "block";
+    }, 5000);
+
 }
 function sort() {
     cleanUp();
     console.log();
+}
+
+function nothing() {
+    let x = 1+1;
+    x = x + 1;
 }
